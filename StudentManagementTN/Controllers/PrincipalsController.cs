273710e6c3265526802758f87c3f427805aa4eff
Sudpool98 <sudarshanrprabhu@gmail.com>
2024-06-21 13:35:10 +0530

    Controllers and Views scaffolding(Student, Teacher).

---
+++ StudentManagementTN/Controllers/PrincipalsController.cs
@@ -0,0 +1,20 @@
+﻿using StudentManagementTN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagementTN.Controllers
+{
+    public class PrincipalsController : Controller
+    {
+        private StudentManagementDBContext db = new StudentManagementDBContext();
+
+        // GET: Principals
+        public ActionResult Portal()
+        {
+            return View();
+        }
+    }
+}
