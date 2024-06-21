50c02622844901afe9bae9be6fc597b28f03f050
Sudpool98 <sudarshanrprabhu@gmail.com>
2024-06-22 02:08:29 +0530

    Portal action created in PrincipalController.

--- StudentManagementTN/Controllers/PrincipalsController.cs
+++ StudentManagementTN/Controllers/PrincipalsController.cs
@@ -1,7 +1,10 @@
 ﻿using StudentManagementTN.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,6 +16,22 @@ namespace StudentManagementTN.Controllers
 
         public ActionResult Portal()
         {
+            var students = db.Students.Include(s => s.ClassDivision).Include(s => s.EduStatus);
+            List<string> excellentlist = new List<string>();
+            List<string> averagelist = new List<string>();
+            List<string> poorlist = new List<string>();
+            foreach (var student in students)
+            {
+                if(student.EduStatus.Rank == 1)
+                    excellentlist.Add(student.ClassDivision.Combined);
+                if (student.EduStatus.Rank == 3)
+                    averagelist.Add(student.ClassDivision.Combined);
+                if (student.EduStatus.Rank == 4)
+                    poorlist.Add(student.ClassDivision.Combined);
+            }
+            ViewBag.Excellent = excellentlist.GroupBy(i => i).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).First();
+            ViewBag.Average = averagelist.GroupBy(i => i).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).First();
+            ViewBag.Poor = poorlist.GroupBy(i => i).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).First();
             return View();
         }
         public ActionResult Login()
