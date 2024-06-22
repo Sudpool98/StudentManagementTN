cc3f640c761c2984565cf245a4e2fb7cfb345f34
Sudpool98 <sudarshanrprabhu@gmail.com>
2024-06-22 13:23:38 +0530

    Correction in HomeController.

--- StudentManagementTN/Controllers/HomeController.cs
+++ StudentManagementTN/Controllers/HomeController.cs
@@ -10,7 +10,17 @@ namespace StudentManagementTN.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                if ((int)Session["logged_in"] == 1)     //If Home Page is accessed while already logged in, logout.
+                {
+                    Session["logged_in"] = 0;
+                    Session["id"] = 0;
+                    Session["user"] = 0;
+                }
+                return View();
+            }
+            catch (NullReferenceException) { return View(); }
         }
     }
 }
